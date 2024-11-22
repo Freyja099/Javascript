943866d6ca9a771d4c7cbf9e7fb42c56106b40dd
Aya Idrissi <aya.zoujari.id@gmail.com>
2024-11-22 17:57:55 +0100

    variables

--- Variables.js
+++ Variables.js
@@ -1,1 +1,13 @@
 // * Session 3 : Variables
+
+// * A variable is a container, we can save a value and then use it later
+
+// * The word " let " creates a new variable 
+// * Syntaxe : let variableName = value;
+// * Variable Name restrictions : special words like let , 
+// * We can't start with a number like 1name ,
+// * We can't use special characters like #&!@*^% except $ and _
+
+// * Javascript has a special feature called Semicolon insertion , that mean it will try to insert the last semicolon in the line of code automatically 
+
+// * when re-assigning a variable we don't use let
