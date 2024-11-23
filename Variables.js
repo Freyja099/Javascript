3f5df6aa3713f4e8e19e9d1d68d3f90d6bd2648f
Aya Idrissi <aya.zoujari.id@gmail.com>
2024-11-23 15:10:52 +0100

    Variables

--- Variables.js
+++ Variables.js
@@ -11,3 +11,11 @@
 // * Javascript has a special feature called Semicolon insertion , that mean it will try to insert the last semicolon in the line of code automatically 
 
 // * when re-assigning a variable we don't use let
+
+// * if we want to create a constant variable we use const instead of let
+
+// * Another way to create a variable is by using " var " it's like let but it has some issues it was used in the older JavaScript versions
+
+// * It's better to use const by default and if we have to change a variable then use let
+
+// * We can know the type of a variable by using typeof variable inside the console log, ex : console.log(typeof variable) ;
